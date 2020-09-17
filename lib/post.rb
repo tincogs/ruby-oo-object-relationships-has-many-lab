c956054aaafec8a0b3c6a02c127d902144b40289
tincogs <tomoday91@gmail.com>
2020-09-16 22:23:57 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+    attr_accessor :author, :title
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        self.author.name if self.author
+    end
+
+end
